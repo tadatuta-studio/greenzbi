0a10a968333169c2c35dd9b4f5fa7c6da617b74b
Vladimir Grinenko <i@tadatuta.com>
2015-05-24 23:25:46 +0300

    Remove desktop level

--- .enb/make.js
+++ .enb/make.js
@@ -30,8 +30,7 @@ var techs = {
         { path: 'libs/bem-components/desktop.blocks', check: false },
         { path: 'libs/bem-components/design/common.blocks', check: false },
         { path: 'libs/bem-components/design/desktop.blocks', check: false },
-        'common.blocks',
-        'desktop.blocks'
+        'common.blocks'
     ];
 
 module.exports = function(config) {
